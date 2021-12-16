d1965e328c0ab8315dbf9579310ab9964884b225
danielbs74 <40871497+danielbs74@users.noreply.github.com>
2021-12-16 12:07:02 +0100

    Add files via upload

---
+++ ex2.R
@@ -0,0 +1,49 @@
+rm(list = ls())
+#We assume A = 0 WLOG
+
+#####2.2#####
+
+r = 0.04
+z = 0
+
+alpha = seq(0.01, 0.99, 0.01)
+x = ((1+r)/(alpha*exp(z)))^(1/(alpha - 1))
+
+plot(x = alpha, y = x, type = 'l', 
+     xlab = expression(alpha), ylab = 'Optimal X',  main = "z = 0")
+
+
+#####2.3#####
+
+r = 0.04
+z = 0.03
+
+alpha = seq(0.01, 0.99, 0.01)
+x = ((1+r)/(alpha*exp(z)))^(1/(alpha - 1))
+
+plot(x = alpha, y = x, type = 'l', 
+     xlab = expression(alpha), ylab = 'Optimal X', main = "z = 0.03")
+
+
+z = 0.05
+
+x = ((1+r)/(alpha*exp(z)))^(1/(alpha - 1))
+
+plot(x = alpha, y = x, type = 'l', 
+     xlab = expression(alpha), ylab = 'Optimal X', main = "z = 0.05")
+
+
+#####2.5#####
+
+r = 0.04
+
+z = seq(0, 0.5, 0.5/98)
+
+ddx = -1 + exp(z)/(1+r)
+
+x = ifelse(ddx > 0, 10, 0)
+
+
+plot(x = z, y = x, type = 'l', 
+     xlab = "z", ylab = 'Optimal X', main = substitute(paste(a, " = 1"), list(a = bquote(alpha))))
+
